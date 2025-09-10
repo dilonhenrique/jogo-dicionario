9d2fe8466e9e53a76a3d8aaa21f142868cdda29b
Dilon Henrique <dilonhenrique@gmail.com>
2025-09-09 23:20:49 -0300

    showing options to guess

--- src/lib/components/modules/InGame/InGame.tsx
+++ src/lib/components/modules/InGame/InGame.tsx
@@ -2,6 +2,7 @@ import { useGame } from "@/lib/contexts/GameContext"
 import DefinitionStage from "./Stages/DefinitionStage";
 import WordSelector from "./Stages/WordSelector";
 import { PlayerList } from "../Player/PlayerList";
+import GuessStage from "./Stages/GuessStage";
 
 export default function InGame() {
   const { stage, players } = useGame();
@@ -12,6 +13,7 @@ export default function InGame() {
 
       {stage === "word_pick" && <WordSelector />}
       {stage === "definition" && <DefinitionStage />}
+      {stage === "guess" && <GuessStage />}
     </div>
   )
 }
---
+++ src/lib/components/modules/InGame/Stages/GuessStage.tsx
@@ -0,0 +1,20 @@
+import { shuffle } from "lodash";
+import { useGame } from "@/lib/contexts/GameContext"
+
+export default function GuessStage() {
+  const { currentRound } = useGame();
+
+  if (!currentRound) return <></>;
+
+  const allDefinitions = [currentRound.word, ...currentRound.fakes];
+
+  return (
+    <ul className="flex flex-col gap-2">
+      {shuffle(allDefinitions).map(word => (
+        <li key={word.id}>
+          {word.definition}
+        </li>
+      ))}
+    </ul>
+  )
+}
