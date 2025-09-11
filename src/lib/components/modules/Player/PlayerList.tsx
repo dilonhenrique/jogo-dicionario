7c8f1dd7e60ff8dfb2fbeb1b1f96db45a2f00d39
Dilon Henrique <dilonhenrique@gmail.com>
2025-09-11 08:57:47 -0300

    ordering players list

--- src/lib/components/modules/Player/PlayerList.tsx
+++ src/lib/components/modules/Player/PlayerList.tsx
@@ -1,13 +1,28 @@
+import { useRoomChannel } from "@/lib/contexts/RoomContext";
 import { Player } from "@/types/user";
 import { Chip } from "@heroui/react";
+import { sortBy } from "lodash";
+import { useMemo } from "react";
 
 type Props = {
   players: Player[];
+  disableMeFirst?: boolean;
 }
 
-export function PlayerList({ players }: Props) {
+export function PlayerList({ players, disableMeFirst }: Props) {
+  const { currentUser } = useRoomChannel();
+
+  const me = players.find(u => u.id === currentUser.id);
+  const others = players.filter(u => u.id !== currentUser.id);
+
+  const list = useMemo(() => {
+    if (disableMeFirst || !me) return sortBy(players, "name");
+
+    return [me, ...sortBy(others, "name")]
+  }, [disableMeFirst, players, me, others]);
+
   return <ul className="flex flex-col gap-2">
-    {players.map(player =>
+    {list.map(player =>
       <Chip
         key={player.id}
         size="sm"
