1c5d6306b90fdbefb100351ddd028b8c174c9047
Dilon Henrique <dilonhenrique@gmail.com>
2025-09-11 08:51:44 -0300

    generating random room code

--- src/lib/pages/Home/HomePage.tsx
+++ src/lib/pages/Home/HomePage.tsx
@@ -1,23 +1,45 @@
 "use client";
 
 import Container from "@/lib/components/ui/Container/Container";
-import { Button, Input } from "@heroui/react";
+import { generateRoomCode, isValidRoomCode } from "@/lib/utils/generateRoomCode";
+import { Button, Form, Input } from "@heroui/react";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
+  const router = useRouter();
+
+  function createNewRoom() {
+    const code = generateRoomCode();
+    router.push(`/r/${code}`);
+  }
+
   return (
     <Container>
-      Home
+      <h1>
+        Jogo do dicionário
+      </h1>
 
-      <div className="flex">
-        <Input placeholder="Número da sala" />
-        <Button>Entrar</Button>
-      </div>
+      <Form
+        action={(formData) => {
+          const { code } = Object.fromEntries(formData);
+
+          if (isValidRoomCode(code)) {
+            router.push(`/r/${code}`);
+          }
+        }}
+
+      >
+        <Input
+          placeholder="Número da sala"
+          name="code"
+          validate={(data) => isValidRoomCode(data) ? true : "Código inválido"}
+        />
+        <Button type="submit">Entrar</Button>
+      </Form>
 
       <Button
         color="primary"
-        onPress={() => {
-          
-        }}
+        onPress={createNewRoom}
       >
         Nova sala
       </Button>
--- src/lib/utils/generateRoomCode.ts
+++ src/lib/utils/generateRoomCode.ts
@@ -1,7 +1,13 @@
-export function generateRoomCode() {
-  return "123456";
+import { sample, times, isString } from "lodash";
+
+const CODE_LENGTH = 8;
+const ALPHANUM = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+export function generateRoomCode(): string {
+  return times(CODE_LENGTH, () => sample(ALPHANUM)).join("");
 }
 
-export function isValidRoomCode(code: string): boolean {
-  return code === "123456";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isValidRoomCode(code: any): code is string {
+  return isString(code) && /^[0-9A-Za-z]{8}$/.test(code);
 }
