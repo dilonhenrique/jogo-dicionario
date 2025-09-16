921beb582bb86c2f2cf7fd7d73f2e7c06620a2eb
Dilon Henrique <dilonhenrique@gmail.com>
2025-09-16 12:31:33 -0300

    centralized container

--- src/lib/components/ui/Container/Container.tsx
+++ src/lib/components/ui/Container/Container.tsx
@@ -6,7 +6,7 @@ type Props = PropsWithChildren & {
 }
 
 export default function Container({ children, className }: Props) {
-  return (<div className={cn("flex flex-col gap-4 p-10 min-h-full max-w-2xl", className)}>
+  return (<div className={cn("flex flex-col gap-4 p-10 min-h-full max-w-2xl mx-auto", className)}>
     {children}
   </div>)
 }
