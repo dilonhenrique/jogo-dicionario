5e2e3b99f37e26daa89234466527e73c3c0503fb
Dilon Henrique <dilonhenrique@gmail.com>
2025-09-19 09:41:40 -0300

    extending game time while playing

--- src/server/services/gameSession/updateState.ts
+++ src/server/services/gameSession/updateState.ts
@@ -1,8 +1,10 @@
 "use server";
 
 import { gameSessionRepo } from "@/server/repositories/gameSession";
+import { roomRepo } from "@/server/repositories/room";
 import { GameState } from "@/types/game";
 
 export default async function updateState(roomCode: string, state: GameState) {
-  return await gameSessionRepo.updateState(roomCode, state);
+  await gameSessionRepo.updateState(roomCode, state);
+  await roomRepo.extend(roomCode);
 }
