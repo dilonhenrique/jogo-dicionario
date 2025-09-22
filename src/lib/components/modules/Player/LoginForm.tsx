20f029d8931fae3a89e93fabaad5cd3d269af307
Dilon Henrique <dilonhenrique@gmail.com>
2025-09-22 00:57:12 -0300

    improving RoomPage readability

---
+++ src/lib/components/modules/Player/LoginForm.tsx
@@ -0,0 +1,38 @@
+import { useSession } from "@/lib/contexts/SessionContext";
+import { addToast, Button, Form, Input } from "@heroui/react";
+import { ArrowRight } from "lucide-react";
+
+export default function LoginForm() {
+  const { createUser } = useSession();
+
+  return (
+    <Form
+      className="flex flex-col"
+      action={async (formData) => {
+        const name = formData.get("name");
+        if (typeof name === "string" && name.length > 3) {
+          createUser({ name });
+        } else {
+          addToast({
+            color: "danger",
+            title: "Nome inválido",
+          })
+        }
+      }}
+    >
+      <h4>Identifique-se para entrar</h4>
+
+      <div className="flex gap-2 w-full">
+        <Input name="name" placeholder="Seu nome" size="lg" />
+        <Button
+          type="submit"
+          color="primary"
+          size="lg"
+          endContent={<ArrowRight className="shrink-0" size={18} />}
+        >
+          Entrar
+        </Button>
+      </div>
+    </Form>
+  );
+}
--- src/lib/pages/Room/RoomPage.tsx
+++ src/lib/pages/Room/RoomPage.tsx
@@ -1,12 +1,12 @@
 "use client";
 
+import LoginForm from "@/lib/components/modules/Player/LoginForm";
 import Room from "@/lib/components/modules/Room/Room";
 import PageContainer from "@/lib/components/ui/Container/PageContainer";
 import { RoomChannelProvider } from "@/lib/contexts/RoomContext";
 import { useSession } from "@/lib/contexts/SessionContext";
 import { RoomComplete } from "@/types/room";
-import { addToast, Button, Divider, Form, Input, Spinner } from "@heroui/react";
-import { ArrowRight } from "lucide-react";
+import { Button, Divider, Spinner } from "@heroui/react";
 import Link from "next/link";
 import { useIsClient } from "usehooks-ts";
 
@@ -15,63 +15,43 @@ type Props = {
 }
 
 export default function RoomPage({ room }: Props) {
-  const { user, createUser } = useSession();
+  const { user } = useSession();
   const isClient = useIsClient();
 
   const amIInThisGame = !room.session || room.session.players.some(u => u.id === user?.id);
 
+  function renderContent() {
+    if (!isClient) {
+      return <Spinner size="sm" />;
+    }
+
+    if (!amIInThisGame) {
+      return (
+        <>
+          <p>Este jogo já começou (sem você)</p>
+          <Button as={Link} href="/">Página inicial</Button>
+        </>
+      );
+    }
+
+    if (user === null) {
+      return <LoginForm />;
+    }
+
+    return (
+      <RoomChannelProvider room={room}>
+        <Room />
+      </RoomChannelProvider>
+    );
+  };
+
   return (
-    <PageContainer className="relative min-h-dvh pb-36">
+    <PageContainer className="relative min-h-dvh pb-36 items-start">
       <h1 className="!text-large text-primary my-2">Sala #{room.code}</h1>
 
       <Divider className="mb-2" />
 
-      {!isClient
-        ? <Spinner />
-        : !amIInThisGame
-          // TODO: improve this ifs
-          ? (
-            <>
-              <p>O jogo já começou sem você</p>
-              <Button as={Link} href="/">Página inicial</Button>
-            </>
-          )
-          : user === null
-            ? (
-              <Form
-                className="flex flex-col"
-                action={async (formData) => {
-                  const name = formData.get("name");
-                  if (typeof name === "string" && name.length > 3) {
-                    createUser({ name });
-                  } else {
-                    addToast({
-                      color: "danger",
-                      title: "Nome inválido",
-                    })
-                  }
-                }}
-              >
-                <h4>Identifique-se para entrar</h4>
-
-                <div className="flex gap-2 w-full">
-                  <Input name="name" placeholder="Seu nome" size="lg" />
-                  <Button
-                    type="submit"
-                    color="primary"
-                    size="lg"
-                    endContent={<ArrowRight className="shrink-0" size={18} />}
-                  >
-                    Entrar
-                  </Button>
-                </div>
-              </Form>
-            )
-            : (
-              <RoomChannelProvider room={room}>
-                <Room />
-              </RoomChannelProvider>
-            )}
+      {renderContent()}
     </PageContainer>
   )
 }
