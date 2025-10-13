f606eb49608579ca5e887d2b8c2003e8a351470a
Dilon Henrique <dilonhenrique@gmail.com>
2025-10-12 23:26:03 -0300

    refactor: optimize leaderboard player points calculation and improve rendering logic

--- src/lib/components/modules/Player/Leaderboard.tsx
+++ src/lib/components/modules/Player/Leaderboard.tsx
@@ -11,27 +11,27 @@ type Props = {
 }
 
 export default function Leaderboard({ players, disableLeaderColor, roundPoints }: Props) {
-  const list = sortByPoints(players);
+  const roundPlayers = players.map(player => ({ ...player, points: player.points + (roundPoints?.get(player.id) || 0) }));
+  const list = sortByPoints(roundPlayers);
 
   return (
     <ul className="flex flex-col gap-4 divide-y divide-foreground-100">
       {list.map((player, index) => {
         const pointsGained = roundPoints?.get(player.id) || 0;
-        
+
         return (
           <li key={player.id} className="flex gap-4 justify-between items-center pb-4">
             <PlayerChip player={player} size="lg" classNames={{ base: "border-none px-0" }} />
 
             <div className="flex items-center gap-2">
-              <p className={disableLeaderColor ? "text-default-400" : getPositionColor(index + 1)}>
-                {player.points} pts
-              </p>
-              
               {pointsGained > 0 && (
                 <Chip size="sm" color="success" variant="flat">
                   +{pointsGained}
                 </Chip>
               )}
+              <p className={disableLeaderColor ? "text-default-400" : getPositionColor(index + 1)}>
+                {player.points} pts
+              </p>
             </div>
           </li>
         );
