c7a6e4a5fe3c45f72c76403dccd42f3e689bf447
Dilon Henrique <dilonhenrique@gmail.com>
2025-11-16 21:41:20 -0300

    feat: add NotFound page with a link to the home page

--- src/app/r/[roomCode]/not-found.tsx
+++ src/app/r/[roomCode]/not-found.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import PageContainer from "@/lib/components/ui/Container/PageContainer";
+import { Button } from "@heroui/react";
 import { DoorOpen } from "lucide-react";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <PageContainer className="text-center items-center justify-center h-dvh">
       <DoorOpen size={200} strokeWidth={1} className="text-primary" style={{ marginTop: "-10%" }} />
       <h1>Sala não encontrada!</h1>
+
+      <Button as={Link} href="/" color="primary" className="mt-10">
+        Página inicial
+      </Button>
     </PageContainer>
   )
 }
